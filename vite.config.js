9e244398ef064909f19432a96d611fb319523e3b
klu2500030136 <2500030136@kluniversity.in>
2026-03-28 00:22:25 +0530

    ready for versel

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+// detect environment
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/LPTD/",
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  base: isGithubPages ? '/LPTD/' : '/',  // ✅ dynamic base
+});
